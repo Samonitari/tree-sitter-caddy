f620af8837008a8ffb838ed0ab4eb5bad9326669
Samonitari <kr.git@mszk.eu>
2023-03-03 10:36:17 +0100

    clean up reduntant query settings a bit

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,26 +1,12 @@
 (snippet_name) @function
 
-(directive_header directive_type: "header" @keyword)
-(directive_abort directive_type: "abort" @keyword)
-(directive_bind directive_type: "bind" @keyword)
-(directive_encode directive_type: "encode" @keyword)
-(directive_file_server directive_type: "file_server" @keyword)
-(directive_handle directive_type: "handle" @keyword)
-(directive_handle_path directive_type: "handle_path" @keyword)
-(directive_import directive_type: "import" @keyword)
-(directive_log directive_type: "log" @keyword)
-(directive_redir directive_type: "redir" @keyword)
-(directive_request_body directive_type: "request_body" @keyword)
-(directive_reverse_proxy directive_type: "reverse_proxy" @keyword)
-(directive_root directive_type: "root" @keyword)
-(directive_tls directive_type: "tls" @keyword)
+(_ directive_type: _ @keyword)
 
-(log_option_format log_option_type: "format" @function.method)
-(log_option_output log_option_type: "output" @function.method)
-(log_option_level log_option_type: "level" @function.method)
+(_ log_option_type: _ @function.method)
 
-(matcher_path_regexp matcher_type: "path_regexp" @function.macro)
-(matcher_path matcher_type: "path" @function.macro)
+(_ file_server_option_browse: _ @function.method)
+
+(_ matcher_type: _ @function.macro)
 
 [
   "max_size"
